e11909edeeddd9f43003d428439ed189c25fd260
laobubu <laobubu@live.cn>
2017-12-13 17:32:57 +0800

    Add command line /help and /hide support

--- WinMate.cpp
+++ WinMate.cpp
@@ -7,9 +7,31 @@ using namespace System;
 using namespace System::Windows;
 using namespace WinMate;
 
-int main() {
-	Application::Run(gcnew MainForm);
+static int execMain(array<String^> ^ args) {
+	MainForm ^ win = gcnew MainForm;
+
+	String ^ args_s = String::Join(" ", args);
+
+	if (args_s->Contains(L"hide")) win->Hide();
+	if (args_s->Contains(L"help")) {
+		MessageBox::Show(
+			L"Parameters: \n"
+			L"  /help     Display these message \n"
+			L"  /hide     Hide main window when starts.");
+		return 0;
+	}
+
+	Application::Run(win);
 	return 0;
+}
+
+int main(int argc, TCHAR **argv) {
+	array<String^> ^ args = gcnew array<String^>(argc - 1);
+	for (int i = 0; i < argc - 1; i++) {
+		args[i] = gcnew String(argv[i + 1]);
+	}
+
+	return execMain(args);
 }
 
 int APIENTRY WinMain(HINSTANCE hInstance,
@@ -17,6 +39,8 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	LPSTR    lpCmdLine,
 	int      nCmdShow)
 {
-	Application::Run(gcnew MainForm);
-	return 0;
+	String^ args_str = gcnew String(lpCmdLine);
+	array<String^> ^ args = args_str->Split();
+
+	return execMain(args);
 }
